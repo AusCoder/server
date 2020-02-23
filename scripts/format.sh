c55a562c4769d61522c0d1c88a399128507c849b
AusCoder <sebmuellermath@gmail.com>
2020-02-24 09:00:04 +1100

    feat(scripts): add format script

---
+++ scripts/format.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -eu
+
+# Trim whitespace
+sed -i -e 's/\s\+$//g' include/*.h
+sed -i -e 's/\s\+$//g' src/*.c
+
+# Eat trailing whitespace
+perl -pi -e "chomp if eof" include/*.h
+perl -pi -e "chomp if eof" src/*.c
+
+# Format code
+clang-format -i include/*.h
+clang-format -i src/*.c
